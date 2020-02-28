69d67296e5f48f9028f65c3e455cdfd6aa7ad3c5
William Barnard <willbarnard687@pm.me>
2020-02-28 11:33:43 +0000

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!, hey mrwillbarnz"
+    return "Hello World!, hello mrwillbarnz"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
