2b25eae65e78af7f0f2ed56543848c50dda9fd2b
William Barnard <willbarnard687@pm.me>
2020-02-28 11:55:04 +0000

    Update app.py
    Added new statement to hello() function.

--- python/app.py
+++ python/app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!, hello mrwillbarnz"
+    return "This is the simplest Python App ever!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
