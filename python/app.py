3165ea2593ec5cd2414f695fef3d8d1dedeef549
William Barnard <willbarnard687@pm.me>
2020-02-28 11:59:45 +0000

    Update app.py

--- python/app.py
+++ python/app.py
@@ -6,6 +6,7 @@ def hello():
     return "Hello World!, hello mrwillbarnz"
     return "This is the simplest Python App ever!"
     return "This is a new line!"
+    return "This is another line!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
