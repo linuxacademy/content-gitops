a077071fb6e7fd16286fa21e696fa871cf0a2bc2
Michael Wright <wrigh517@gmail.com>
2020-03-19 09:28:22 -0700

    Update app.py

--- python/app.py
+++ python/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Yo yo yo, whatsup"
 
+@app.route("/food")
+def food():
+    return "Indian Buffet"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
