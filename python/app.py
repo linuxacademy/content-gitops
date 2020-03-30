f75a354a4df64bfa38296278c91628e2f91658b8
Splak13 <61446669+Splak13@users.noreply.github.com>
2020-03-30 13:37:04 -0400

    Update app.py

--- python/app.py
+++ python/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 
-    return "Hello LA Students!!! Feature Update"
+    return "Hello LA Students!!! Mergy Merge"
 
 
 if __name__ == "__main__":
