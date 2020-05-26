539e3fc822e19d4fbd26b20d108b5865cb8c6ccc
Artur Kornatowski <artur.kornatowski@gmail.com>
2020-05-26 19:32:46 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello again Artur!"
+    return "Hello again Artur featuristic!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
