0dbe316b17808ca245985f755c9b79343607cb14
Mike <55143479+mikehsieh97@users.noreply.github.com>
2020-07-05 17:08:19 -0400

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello My ACT New World!"
+    return "Hello My ACT2 New World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
