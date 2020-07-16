6784fbe7f9c1b64f4e83eb8b9f7606f0dc7701b9
yatin2020 <68281759+yatin2020@users.noreply.github.com>
2020-07-16 12:07:52 -0400

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World Yatin2!"
+    return "Hello World Yatin2f!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
