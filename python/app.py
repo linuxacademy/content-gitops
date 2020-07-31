dd0bed5bd8e86918f235fa174e3c8695889aa145
Jacques-olivier Dordonne <jacques-olivier.dordonne@onespan.com>
2020-07-31 17:33:45 -0400

    ok jacko

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Buju banton is alive!"
+    return "Je m'apelle jacko et je suis le plus beau"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
