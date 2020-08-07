a478b9811dfd52c245118a06c9a6e2747036fbb1
bayaprakash <cbaya.reddy8@gmail.com>
2020-08-07 11:12:09 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello I am Bayaprakash Reddy "
+    return "Hello I am Bayaprakash "
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
