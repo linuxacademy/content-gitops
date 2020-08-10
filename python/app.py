b97202b34ff509552dfa11c31d9b5adc98d608b3
pourehsadram <dylan.murray@hotmail.com>
2020-08-10 19:52:13 +0100

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Dylan - Please Work!"
+    return "Hello Dylan - new ops repository build"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
