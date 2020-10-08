861829a5246d2af12b40899ad40c3feef6a18093
Pal-Mahima <71060832+Pal-Mahima@users.noreply.github.com>
2020-10-08 18:21:22 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello LA Student in new branch!!"
+    return "Hello LA Student from new branch!!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
