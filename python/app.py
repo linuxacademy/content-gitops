a9f39ee1ebf3acf2b911c9189af2a3350c10790e
Caleb Espinoza G <caleb.espinoza@outlook.com>
2020-12-10 21:29:18 -0400

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Jalasoft!"
+    return "Hello DevOps team at Jalasoft!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
