3259adf53802229ae634e87bbb327bab9cb4685a
Konstantin Kuznetsov <kkuznetsov@splunk.com>
2021-01-20 17:19:40 +1100

    Update app.py
    app change 2

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello the Brave New World take 2!"
+    return "Hello the Brave New World take 3!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
