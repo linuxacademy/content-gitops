9674201028c7f4d5f291f92aa4c5cc105a9cb766
Pavel Pavlov <57105764+pavel77pavlov@users.noreply.github.com>
2021-04-27 19:50:05 +0300

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World Poker Stars S !"
+    return "Hello World PokerStars S !"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
