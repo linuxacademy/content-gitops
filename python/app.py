e170ae33408ccdf95a3cd8d85882894cf4d4a873
José Flores <josefloressv@gmail.com>
2021-06-24 01:02:57 -0600

    hellov1.2 CICD with Docker Hub in progres...

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Acloud Guru Test OK after pull request!"
+    return "Hello Acloud Guru Test OK after pull request hellov1.2!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
