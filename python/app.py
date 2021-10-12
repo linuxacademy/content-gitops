d8c6816a2554a3ea4b73a6bf4d3053e3eda093a8
Dogmean <46533971+Dogmean@users.noreply.github.com>
2021-10-12 14:34:08 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello! Hey! Hello, hello! Hello!"
+    return "Away with you, vile beggar!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
