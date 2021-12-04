c3cd080865f72f0497d7110a8a60062b3d6b2c12
jeisson-forero91 <62520146+jeisson-forero91@users.noreply.github.com>
2021-12-03 22:07:48 -0500

    :arrow_up: OPS-001: Update app

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World nooby!"
+    return "Hello World nooby, welcome to the jungle!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
