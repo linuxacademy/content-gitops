2b0a4b92f8a8b6d0b095b872c7388cff886e8d2d
aakram786 <30791579+aakram786@users.noreply.github.com>
2022-01-03 22:55:51 +0000

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello LA Student! Again Again 3rd time lucky wow t"
+    return "Hello LA Student! Pull request"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
