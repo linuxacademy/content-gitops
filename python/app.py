0d029b65ee728044e459a5b3ca153d1fb5142cf0
mohdwazid <73927646+mohdwazid@users.noreply.github.com>
2023-03-20 10:35:01 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello DevOps World! Its a PULL Request Release Gating"
+    return "Hello DevOps World! Its a GitPULLRequestWorkFlow Example"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
