cb3894c4d4c4bda204dfd87d38bea5d85c3e7175
PKConsul <zlodziej.koni@gmail.com>
2023-04-16 20:55:32 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Worlds test!"
+    return "Hello Worlds tests!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
