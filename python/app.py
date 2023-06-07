fa462c9d9aa0ea82eff7d0792435ff818dc527a3
Kompromatas <135023660+Kompromatas@users.noreply.github.com>
2023-06-07 15:19:12 +0100

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "LAbas jei viskas gerai"
+    return "Labas jei viskas gerai, dublis 2"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
